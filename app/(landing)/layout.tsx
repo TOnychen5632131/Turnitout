9455e8f6b9adf0bd0048fa0d162fb552124f3eda
TOnychen5632131 <162278003+TOnychen5632131@users.noreply.github.com>
2024-10-08 02:22:03 -0400

    正确的广告

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -5,27 +5,27 @@ const LandingLayout = ({ children }: PropsWithChildren) => {
   return (
     <>
       <Head>
-        {/* 插入 Meta 验证标签 */}
-        <meta name="verify-admitad" content="f988fb0a8d" />
+        {/* 插入广告 */}
+        <script src="https://ad.admitad.com/b/p173w2r5daf988fb0a8d4bf89f6ddb/"></script>
       </Head>
       <main className="h-full bg-[#111827] overflow-auto">
         <div className="mx-auto max-w-screen-xl h-full w-full">
-          {/* 插入广告代码 */}
+          {/* 广告插入的区域 */}
           <div className="ad-banner">
-            {/* admitad.banner: p173w2r5daf988fb0a8d4bf89f6ddb Tomtop WW */}
-            <a target="_blank" rel="nofollow" href="https://ficca2021.com/g/xljorca896f988fb0a8d4bf89f6ddb/">
+            <a
+              target="_blank"
+              rel="nofollow"
+              href="https://ficca2021.com/g/p173w2r5daf988fb0a8d4bf89f6ddb/?i=4"
+            >
               <img
                 width="120"
                 height="600"
-                style={{ border: "0" }} // 使用 inline 样式来代替 border 属性
+                style={{ border: 0 }}
                 src="https://ad.admitad.com/b/p173w2r5daf988fb0a8d4bf89f6ddb/"
                 alt="Tomtop WW"
               />
             </a>
-            {/* /admitad.banner */}
           </div>
-
-          {/* 页面内容 */}
           {children}
         </div>
       </main>
