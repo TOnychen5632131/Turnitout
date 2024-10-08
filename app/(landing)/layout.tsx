3dc237835d587001b3ee3bcbde2e13bd86911986
TOnychen5632131 <162278003+TOnychen5632131@users.noreply.github.com>
2024-10-08 01:45:04 -0400

    ad

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,10 +1,17 @@
+import Head from "next/head";
 import type { PropsWithChildren } from "react";
 
 const LandingLayout = ({ children }: PropsWithChildren) => {
   return (
-    <main className="h-full bg-[#111827] overflow-auto">
-      <div className="mx-auto max-w-screen-xl h-full w-full">{children}</div>
-    </main>
+    <>
+      <Head>
+        {/* 插入 Meta 验证标签 */}
+        <meta name="verify-admitad" content="f988fb0a8d" />
+      </Head>
+      <main className="h-full bg-[#111827] overflow-auto">
+        <div className="mx-auto max-w-screen-xl h-full w-full">{children}</div>
+      </main>
+    </>
   );
 };
 
