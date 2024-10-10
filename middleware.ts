8e6f7814cfaff43c61281c80bcb2adb366e58bae
TOnychen5632131 <162278003+TOnychen5632131@users.noreply.github.com>
2024-10-09 21:18:50 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,13 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook", "/dashboard"], // 添加 "/dashboard" 到 publicRoutes 中
+  afterAuth: (auth, req) => {
+    // 如果用户没有登录，并且尝试访问 "/settings"，重定向到登录页面
+    if (!auth.userId && req.nextUrl.pathname === "/settings") {
+      return NextResponse.redirect("/sign-in");
+    }
+  },
 });
 
 export const config = {
