2e2c24c8a54141575bf9770c1739980ac50cbf15
TOnychen5632131 <162278003+TOnychen5632131@users.noreply.github.com>
2024-10-09 21:21:57 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,12 @@ import { NextResponse } from "next/server";
 
 export default authMiddleware({
   afterAuth: (auth, req) => {
+    const url = req.nextUrl.clone();
+    const baseUrl = `${url.protocol}//${url.host}`;  // 动态获取主机地址
+
     // 如果用户没有登录，并且尝试访问 "/settings"，重定向到登录页面
     if (!auth.userId && req.nextUrl.pathname === "/settings") {
-      return NextResponse.redirect("/sign-in");
+      return NextResponse.redirect(`${baseUrl}/sign-in`);  // 绝对 URL
     }
   },
 });
