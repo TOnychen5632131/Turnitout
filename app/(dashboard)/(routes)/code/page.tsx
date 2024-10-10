0357fd7a8ab6c29f542a49717a732e1b66fdc86b
TOnychen5632131 <162278003+TOnychen5632131@users.noreply.github.com>
2024-10-09 20:09:35 -0400

    Update page.tsx

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -169,9 +169,9 @@ const CodePage = () => {
               <Loader />
             </div>
           )}
-          {messages.length === 0 && !isLoading && (
+          {/* {messages.length === 0 && !isLoading && (
             <Empty label="No conversation started." />
-          )}
+          )} */}
           <div className="flex flex-col-reverse gap-y-4">
             {messages.map((message, i) => (
               <div
