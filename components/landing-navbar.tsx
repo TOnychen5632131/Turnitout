6cdcd4f83a9ec9e7d452782c7bf1600055e96bd1
TOnychen5632131 <162278003+TOnychen5632131@users.noreply.github.com>
2024-10-10 01:02:02 -0400

    中文文案修改

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -32,7 +32,7 @@ export const LandingNavbar = () => {
 
       <div className="flex items-center gap-x-2">
         <Button variant="outline" className="rounded-full" asChild>
-          <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>Get Started</Link>
+          <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>开始使用</Link>
         </Button>
 
         <Button className="rounded-full" asChild>
