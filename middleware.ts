e7ca1e83b60e5240560d052b0eee5ece149f2579
TOnychen5632131 <162278003+TOnychen5632131@users.noreply.github.com>
2024-10-10 09:39:18 -0400

    修复api请求

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,21 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/sign-in", "/sign-up", "/pricing", "/api/stripe"],  // 确保 /api/stripe 路径可以被未登录用户访问
+  publicRoutes: ["/", "/sign-in", "/sign-up", "/pricing", "/api/stripe", "/api/webhook"],  // 确保 /api/stripe 路径可以被未登录用户访问
   ignoredRoutes: ["/api/webhook"],  // 确保 webhook 路径不会被保护
 });
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],  // 路由匹配
 };
+
+
+// import { authMiddleware } from "@clerk/nextjs";
+
+// export default authMiddleware({
+//   publicRoutes: ["/", "/api/webhook"],
+// });
+
+// export const config = {
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
