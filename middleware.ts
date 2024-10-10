4e177bc3692ec52d209e93147222459ed01f9c9a
TOnychen5632131 <162278003+TOnychen5632131@users.noreply.github.com>
2024-10-09 21:27:06 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,13 @@ import { authMiddleware } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export default authMiddleware({
+  publicRoutes: ["/", "/api/webhook", "/settings"],  // 将 /settings 添加到 publicRoutes
   afterAuth: (auth, req) => {
     const url = req.nextUrl.clone();
-    const baseUrl = `${url.protocol}//${url.host}`;  // 动态获取主机地址
+    const baseUrl = `${url.protocol}//${url.host}`;
 
-    // 如果用户没有登录，并且尝试访问 "/settings"，重定向到登录页面
     if (!auth.userId && req.nextUrl.pathname === "/settings") {
-      return NextResponse.redirect(`${baseUrl}/sign-in`);  // 绝对 URL
+      return NextResponse.redirect(`${baseUrl}/sign-in`);
     }
   },
 });
