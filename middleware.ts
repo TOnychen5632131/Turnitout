24692218afb87f27fce18378c0f30baae1f10f46
TOnychen5632131 <162278003+TOnychen5632131@users.noreply.github.com>
2024-10-09 20:34:41 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook"],
+  publicRoutes: ["/", "/api/webhook", "/settings"], // 添加 '/settings'
 });
 
 export const config = {
