5f945bca40a31291f21e4d957f7002177e5abce6
TOnychen5632131 <162278003+TOnychen5632131@users.noreply.github.com>
2024-10-10 00:57:09 -0400

    Update landing-hero.tsx

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -11,14 +11,14 @@ export const LandingHero = () => {
   return (
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
-        <h1>The Best AI Tool for</h1>
+        <h1>留学生最需要的</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
           <TypewriterComponent
             options={{
               strings: [
-                "Chatbot.",
-                "Photo Generation.",
-                "Code Generation.",
+                "降AI率神器",
+                "检查AI率工具",
+                "保持字数改写神器",
               ],
               autoStart: true,
               loop: true,
@@ -28,7 +28,7 @@ export const LandingHero = () => {
       </div>
 
       <div className="text-sm md:text-xl font-light text-zinc-400">
-        Create content using AI 10x faster.
+        让 AI 不在查的出来
       </div>
 
       <div className="">
@@ -38,13 +38,13 @@ export const LandingHero = () => {
           asChild
         >
           <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
-            Start Generating For Free
+            将文章 AI 率降低
           </Link>
         </Button>
       </div>
 
       <div className="text-zinc-400 text-xs md:text-sm font-normal">
-        No credit card required.
+        免费试用
       </div>
     </div>
   );
