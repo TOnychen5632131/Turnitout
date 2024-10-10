2c94a932b6aedab1943fa7a7c7531a17e5150100
TOnychen5632131 <162278003+TOnychen5632131@users.noreply.github.com>
2024-10-09 20:21:40 -0400

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -46,35 +46,35 @@ export const TESTIMONIALS = [
 
 export const TOOLS = [
   {
-    label: "Conversation",
+    label: "降低 Ai 率",
     icon: MessageSquare,
     color: "text-violet-500",
     bgColor: "bg-violet-500/10",
     href: "/conversation",
   },
+  // {
+  //   label: "Music Generation",
+  //   icon: Music,
+  //   color: "text-emerald-500",
+  //   bgColor: "bg-violet-500/10",
+  //   href: "/music",
+  // },
+  // {
+  //   label: "Image Generation",
+  //   icon: ImageIcon,
+  //   color: "text-pink-700",
+  //   bgColor: "bg-pink-700/10",
+  //   href: "/image",
+  // },
+  // {
+  //   label: "Video Generation",
+  //   icon: VideoIcon,
+  //   color: "text-orange-700",
+  //   bgColor: "bg-orange-700/10",
+  //   href: "/video",
+  // },
   {
-    label: "Music Generation",
-    icon: Music,
-    color: "text-emerald-500",
-    bgColor: "bg-violet-500/10",
-    href: "/music",
-  },
-  {
-    label: "Image Generation",
-    icon: ImageIcon,
-    color: "text-pink-700",
-    bgColor: "bg-pink-700/10",
-    href: "/image",
-  },
-  {
-    label: "Video Generation",
-    icon: VideoIcon,
-    color: "text-orange-700",
-    bgColor: "bg-orange-700/10",
-    href: "/video",
-  },
-  {
-    label: "Code Generation",
+    label: "AI 检测",
     icon: Code,
     color: "text-green-700",
     bgColor: "bg-green-700/10",
